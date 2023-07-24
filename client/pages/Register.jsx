78d593068429b45802c3fd3fc69e1ee89c469d66
joshuaautawi <autawijoshua@gmail.com>
2023-07-24 22:35:04 +0700

    add state in register component

--- client/pages/Register.jsx
+++ client/pages/Register.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  console.log(inputs);
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
   return (
     <div className="auth">
       <h1>Register</h1>
       <form>
-        <input type="text" placeholder="username" />
-        <input type="email" placeholder="email" />
-        <input type="passsword" placeholder="password" />
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
 
         <button>Login</button>
         <p>Error !</p>
